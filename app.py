58e24e15d1e63d3f6294e7595c8e58338f8f094f
Quiddiqin <quina.chen99@gmail.com>
2023-05-09 15:20:05 +0200

    Prova template

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    st.title("Template")
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+#streamlit run app.py
